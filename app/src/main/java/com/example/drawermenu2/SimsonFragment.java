7b504bd886ee93296faab97a65bb96047331e826
stepkos <stepkos@wp.pl>
2021-12-06 09:41:10 +0100

    to #12

---
+++ app/src/main/java/com/example/drawermenu2/SimsonFragment.java
@@ -0,0 +1,20 @@
+package com.example.drawermenu2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SimsonFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.simpson_fragment, container, false);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
